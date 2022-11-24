4a3eadc8916676867eaa5943f9c5ac753b16664c
jmmarao <jmmarao@gmail.com>
2022-11-24 20:49:03 -0300

    First frontend test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [state, setState] = useState([]);
+
+  // Sugestão para tratamento de erros (bem simplificado)
+  const [erro, setErro] = useState({
+    hasErro: false,
+    mensagemErro: ""
+  });
+
+  useEffect(
+    () => {
+      // Executar fetch para pegar informações
+      // Necessário criar uma async function pois o fetch é assíncrono (await)
+      async function buscaDados() {
+        const resposta =
+          await fetch('http://localhost:8081/financa').catch((erro) => {
+            // Tratamento de erro de execução
+            console.log("Erro ao realizar o fetch");
+            setErro({
+              hasErro: true,
+              mensagemErro: "Erro ao realizar o fetch"
+            });
+          })
+        // Tratamento de erro de aplicação
+        //console.table(resposta);
+        if (resposta.status >= 200 && resposta.status <= 299) {
+          // Caso der tudo certo é executado esse bloco de código
+          const respostaJson = await resposta.json();
+          setState(respostaJson);
+          console.table(respostaJson);
+        }
+        else {
+          console.log(`Erro! Requisição com código ${resposta.status}`);
+        }
+      }
+
+      buscaDados();
+
+      // Atualizar o state a partir das informações coletadas
+    }, []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Listagem de Finanças</h3>
+      <ul>
+        {state.map((financa, indice) => {
+          return <li key={financa.id}>{financa.descricao}</li>
+        })}
+      </ul>
     </div>
   );
 }
