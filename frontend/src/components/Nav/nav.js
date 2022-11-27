aa527f1998eef00a12c8529f4207a315feadc7ce
jmmarao <jmmarao@gmail.com>
2022-11-27 19:40:36 -0300

    Implement logout

--- frontend/src/components/Nav/nav.js
+++ frontend/src/components/Nav/nav.js
@@ -1,6 +1,11 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
 function Nav() {
+    const deslogar = (e) => {
+        localStorage.clear();
+    }
+
     return (
         <nav>
             <ul className="nav nav-tabs">
@@ -12,6 +17,9 @@ function Nav() {
                 </li>
                 <li className="nav-item">
                     <Link className="nav-link" to='/sobre' >Sobre</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to='/' onClick={deslogar}>Logout</Link>
                 </li>
             </ul>
         </nav>
